dadca1b44bff321c19680a2c1385e21ccb4c2f1a
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-10-24 13:28:15 -0500

    Curso POO

--- POO/CursoPOOUber/Java/Main.java
+++ POO/CursoPOOUber/Java/Main.java
@@ -1,5 +1,8 @@
 class Main{
  public static void main(String[] args){
-    System.out.printIn("Hola Mundo");
+    System.out.println("Hola Mundo");
+    Car car = new Car("AMQ123", new Account("Jose Martinez", "AND123" ));
+    car.passeger = 4;
+    car.printDataCar();
 }
 }
