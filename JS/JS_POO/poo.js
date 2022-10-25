1b056c58dd78e836cee747caa2f77d36449e61a1
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-10-25 08:33:53 -0500

    Default parameters

--- JS/JS_POO/poo.js
+++ JS/JS_POO/poo.js
@@ -47,8 +47,9 @@ const andres = new Student(
     ],
 );
 
-// Utilizando como parámetro objetos
+
 // Prototipos con la sintaxis de clases
+// Utilizando como parámetro objetos
 class Student2 {
     constructor({
       name,
@@ -72,3 +73,36 @@ class Student2 {
     age: 28,
     email: "Maria@gmail.com",
   }); 
+
+  //Parámetros con valores por default 
+
+  class Student3{
+    constructor({
+      name,
+      email, 
+      username,
+      twitter = undefined,
+      instagram = undefined,
+      facebook = undefined,
+      approvedCourses = [],
+      learningPaths = [],
+    }){
+      this.name = name;
+      this.email = email;
+      this.username = username;
+      this.socialMedia ={
+        twitter, 
+        instagram, 
+        facebook,
+      };
+      this.approvedCourses = approvedCourses;
+      this.learningPaths  = learningPaths;
+    }
+  };
+
+  const emilce = new Student3({
+    name: "Emilce",
+    username:"emi",
+    email: "emi@gmail.com",
+    twitter: "emiai",
+  });
