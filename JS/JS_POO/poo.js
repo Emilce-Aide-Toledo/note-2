04aa794a86814eebc4be29a00bc7d2391b64d97a
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-10-25 00:06:06 -0500

    Prototipo con la sintaxis de Clases

--- JS/JS_POO/poo.js
+++ JS/JS_POO/poo.js
@@ -1,3 +1,32 @@
+//Objetos literales
+const ana = {
+    name: "Ana",
+    age: 20,
+    cursosAprobados:[ "HTML y CSS"],
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+}
+
+//Prototipos de funciones
+function Student1(name, age, cursosAprobados){
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+}
+
+Student1.prototype.aprobarCurso = function (nuevoCurso){
+    this.cursosAprobados.push(nuevoCurso);
+}
+
+//Instancias de prototipo
+const juana = new Student1(
+    "Juana",
+    19,
+    ["Matemática Aplicada"],
+);
+
+//Prototipo con la sintaxis de Clases
 class Student{
     constructor(name, age, cursosAprobados){
         this.name = name;
